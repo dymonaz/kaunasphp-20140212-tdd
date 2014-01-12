60876f8b7090dd5f9f02e150b4503cf77b95c31f
Dominykas Blyžė <hello@dominykas.com>
2014-01-13 01:26:06 +0200

    typo

--- server/save.js
+++ server/save.js
@@ -5,14 +5,14 @@ module.exports = function (req, res) {
 	if (fileName.substr(fileName.length - 8) == ".test.js") {
 
 		console.log("Test received, saving: " + fileName);
-		fs.writeFileSync("build/tmp/" + fileName, req.body.value);
+		fs.writeFileSync("build/tmp/" + fileName, req.body.text);
 
 	} else {
 
 		console.log("Implementation received: " + fileName);
 		var outF = parseInt(fileName.substr(0, 3), 10) <= 12 ? "client/resultViewer.js" : "server/results.js";
 		console.log("Saving as " + outF);
-		fs.writeFileSync(outF, req.body.value);
+		fs.writeFileSync(outF, req.body.text);
 
 	}
 	res.send("OK");
