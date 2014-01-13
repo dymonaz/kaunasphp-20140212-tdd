34339c666784f16a30fa0eaa8a57d21c9cf44b94
Dominykas Blyžė <hello@dominykas.com>
2014-01-13 22:27:18 +0200

    styling results/status divs

--- client/index.js
+++ client/index.js
@@ -6,5 +6,7 @@ module.exports = function () {
 
 	[].forEach.call(document.querySelectorAll(".code-editor textarea"), initEditor);
 	Reveal.addEventListener( 'slidechanged', refreshEditor);
+	resultViewer(document.body);
+
 };
 module.exports.resultViewer = resultViewer;
