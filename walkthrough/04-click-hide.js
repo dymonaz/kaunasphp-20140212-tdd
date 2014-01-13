325b2777be9beb26dc333e1003f1c40a2398119d
Dominykas Blyžė <hello@dominykas.com>
2014-01-13 22:17:02 +0200

    04-click-hide

---
+++ walkthrough/04-click-hide.js
@@ -0,0 +1,14 @@
+module.exports = function (elParent)
+{
+  var statusDiv = '<div id="testStatus"></div>';
+  elParent.insertAdjacentHTML("beforeend", statusDiv);
+
+	var resultsDiv = '<div id="testResults"></div>';
+  elParent.insertAdjacentHTML("beforeend", resultsDiv);
+	
+	var elStatus = elParent.querySelector('#testStatus');
+	var elResults = elParent.querySelector('#testResults');
+	elStatus.addEventListener('click', function () {
+		elResults.classList.toggle("visible");
+	});
+};
---
+++ walkthrough/04-click-hide.test.js
@@ -0,0 +1,50 @@
+
+var resultViewer = require("clientApp").resultViewer;
+
+buster.testCase("resultViewer", {
+
+ "setUp": function () {
+	 /*:DOC elParent = <div>
+	 	<span>
+	 		some text
+		</span>
+	 </div> */
+	 resultViewer(this.elParent);
+	 expect(this.elParent.innerHTML).toMatch("some text");
+ },
+								 
+	"should render status div": function ()
+	{
+		assert.match(this.elParent.innerHTML, 
+									'<div id="testStatus"');
+	},
+
+	 "should render results div": function ()
+	{
+		expect(this.elParent.innerHTML)
+			.toMatch('<div id="testResults"');
+	},
+	
+	"when status is clicked, display results": function ()
+	{
+		var statusDiv = this.elParent.querySelector('#testStatus');
+		var resultsDiv = this.elParent.querySelector('#testResults');
+		var evt = document.createEvent("MouseEvent");
+		evt.initEvent("click", true, true, null);
+		statusDiv.dispatchEvent(evt);
+		expect(resultsDiv.classList).toMatch(["visible"]);
+	},
+	
+	"when status is clicked, hide results": function ()
+	{
+		var statusDiv = this.elParent.querySelector('#testStatus');
+		var resultsDiv = this.elParent.querySelector('#testResults');
+		resultsDiv.className="visible";
+		
+		var evt = document.createEvent("MouseEvent");
+		evt.initEvent("click", true, true, null);
+		statusDiv.dispatchEvent(evt);
+		expect(resultsDiv.className).not.toMatch("visible");
+	}
+
+});
