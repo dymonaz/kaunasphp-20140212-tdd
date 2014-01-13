a7340f48db2490d5a398dfb7d52c4c7c50baad37
Dominykas Blyžė <hello@dominykas.com>
2014-01-13 23:12:16 +0200

    this is how final node results.js + tests should look like

--- server/results.js
+++ server/results.js
@@ -1,3 +1,11 @@
+var fs = require("fs");
 module.exports = function (req, res, next) {
-	next();
+	try
+	{
+		var br = fs.readFileSync("build/browser.results.html").toString();
+		var nr = fs.readFileSync("build/node.results.html").toString();
+		res.send([br,nr].join("<hr/>"));
+	} catch (e) {
+		next('route');
+	}
 };
--- server/test/results.test.js
+++ server/test/results.test.js
@@ -1,7 +1,61 @@
+var app = require("../index");
+var supertest = require("supertest"),
+	fs = require("fs");
+
 buster.testCase("results", {
 
-	"should exist": function () {
-		expect(require("../results")).toBeDefined();
+	"setUp": function () {
+		expect(true).toBeTrue();
+	},
+
+	"should serve combined results.html": function (done) {
+
+		var readStub = this.stub(fs, "readFileSync");
+		readStub.withArgs("build/browser.results.html").returns("br");
+		readStub.withArgs("build/node.results.html").returns("nr");
+
+		supertest(app)
+			.get("/results")
+			.expect(200)
+			.expect("br<hr/>nr")
+			.end(function (e) {
+				expect(e).toBeNull();
+				expect(readStub).toHaveBeenCalledTwice();
+				done();
+			});
+
+	},
+
+	"should 404 when no browser results": function (done) {
+
+		var readStub = this.stub(fs, "readFileSync");
+		readStub.withArgs("build/browser.results.html").throws();
+		readStub.withArgs("build/node.results.html").returns("nr");
+
+		supertest(app)
+			.get("/results")
+			.expect(404)
+			.end(function (e) {
+				expect(e).toBeNull();
+				done();
+			});
+
+	},
+
+	"should 404 when no node results": function (done) {
+
+		var readStub = this.stub(fs, "readFileSync");
+		readStub.withArgs("build/browser.results.html").returns("br");
+		readStub.withArgs("build/node.results.html").throws();
+
+		supertest(app)
+			.get("/results")
+			.expect(404)
+			.end(function (e) {
+				expect(e).toBeNull();
+				done();
+			});
+
 	}
 
 });
