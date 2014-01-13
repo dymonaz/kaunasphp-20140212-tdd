132523a9d822b3cd2835a14ee5ae357b7654c6b0
Dominykas Blyžė <hello@dominykas.com>
2014-01-14 00:24:25 +0200

    loadResults() tests will have to look something like this

--- client/resultViewer.js
+++ client/resultViewer.js
@@ -1,3 +1,41 @@
-module.exports = {
+var elStatus, elResults;
+
+module.exports.init = function (elParent) {
+	var statusDiv = '<div id="testStatus"></div>';
+	elParent.insertAdjacentHTML("beforeend", statusDiv);
+
+	var resultsDiv = '<div id="testResults"></div>';
+	elParent.insertAdjacentHTML("beforeend", resultsDiv);
+
+	elStatus = elParent.querySelector('#testStatus');
+	elResults = elParent.querySelector('#testResults');
+
+	elStatus.addEventListener('click', function () {
+		elResults.classList.toggle("visible");
+	});
+};
+
+var reqwest = require("reqwest");
+module.exports.loadResults = function (done) {
+	elStatus.dataset.status = "pending";
+	elResults.innerHTML = "<p>Loading...</p>";
+
+	return reqwest({
+		url: "/results",
+		type: "html",
+		success: function (res)
+		{
+			module.exports.onReceived(null, res);
+			if (done) done();
+		},
+		error: function ()
+		{
+			module.exports.onReceived(new Error());
+			if (done) done();
+		}
+	});
+};
+
+module.exports.onReceived = function (e, res) {
 
 };
--- client/test/resultViewer.test.js
+++ client/test/resultViewer.test.js
@@ -2,9 +2,125 @@ var resultViewer = require("clientApp").resultViewer;
 
 buster.testCase("resultViewer", {
 
-	"should exist": function ()
-	{
-		expect(resultViewer).toBeDefined();
+	"setUp": function () {
+		/*:DOC elParent = <div>
+		    <span>
+		        some text
+		    </span>
+		 </div>
+		 */
+		resultViewer.init(this.elParent);
+		expect(this.elParent.innerHTML).toMatch("some text");
+	},
+
+	"should render status div": function () {
+		assert.match(this.elParent.innerHTML,
+			'<div id="testStatus"');
+	},
+
+	"should render results div": function () {
+		expect(this.elParent.innerHTML)
+			.toMatch('<div id="testResults"');
+	},
+
+	"when status is clicked, display results": function () {
+		var statusDiv = this.elParent.querySelector('#testStatus');
+		var resultsDiv = this.elParent.querySelector('#testResults');
+		var evt = document.createEvent("MouseEvent");
+		evt.initEvent("click", true, true, null);
+		statusDiv.dispatchEvent(evt);
+		expect(resultsDiv.classList).toMatch(["visible"]);
+	},
+
+	"when status is clicked, hide results": function () {
+		var statusDiv = this.elParent.querySelector('#testStatus');
+		var resultsDiv = this.elParent.querySelector('#testResults');
+		resultsDiv.className = "visible";
+
+		var evt = document.createEvent("MouseEvent");
+		evt.initEvent("click", true, true, null);
+		statusDiv.dispatchEvent(evt);
+		expect(resultsDiv.className).not.toMatch("visible");
+	},
+
+	"// ??? should loadResults()": function () {
+
+	},
+
+	"loadResults()": {
+		"should change status to loading": function () {
+			this.useFakeServer();
+
+			resultViewer.loadResults();
+			var statusDiv = this.elParent.querySelector('#testStatus');
+			expect(statusDiv.dataset.status).toEqual("pending");
+			var resultsDiv = this.elParent.querySelector('#testResults');
+			expect(resultsDiv.innerHTML).toMatch("<p>Loading...</p>");
+		},
+
+		"should XHR, then onReceived": function () {
+			var onReceivedStub = this.stub(resultViewer, "onReceived");
+
+			var server = this.useFakeServer();
+			server.respondWith("GET", "/results", [200, {"content-type": "text/html"}, "RESULTS"]);
+
+			resultViewer.loadResults();
+			expect(server.requests.length).toEqual(1);
+
+			server.respond();
+			expect(onReceivedStub).toHaveBeenCalledOnceWith(null, "RESULTS");
+		},
+
+		"should XHR, then onReceived (server)": function () {
+			var onReceivedStub = this.stub(resultViewer, "onReceived");
+
+			var server = this.useFakeServer();
+			server.respondWith("GET", "/results", [404, {"content-type": "text/html"}, ""]);
+
+			resultViewer.loadResults();
+			expect(server.requests.length).toEqual(1);
+
+			server.respond();
+			expect(onReceivedStub).toHaveBeenCalledOnce();
+		},
+
+		"should XHR, then onReceived (async)": function (done) {
+			var onReceivedStub = this.stub(resultViewer, "onReceived");
+
+			var server = this.useFakeServer();
+			server.respondWith("GET", "/results", [200, {"content-type": "text/html"}, "RESULTS"]);
+
+			resultViewer.loadResults(function () {
+
+				expect(onReceivedStub).toHaveBeenCalledOnceWith(null, "RESULTS");
+				done();
+
+			});
+
+			expect(server.requests.length).toEqual(1);
+			setTimeout(function () {
+				server.respond();
+			}, 100);
+
+		},
+
+		"should XHR, then onReceived (promise)": function () {
+			var onReceivedStub = this.stub(resultViewer, "onReceived");
+
+			var server = this.useFakeServer();
+			server.respondWith("GET", "/results", [200, {"content-type": "text/html"}, "RESULTS"]);
+
+			var promise = resultViewer.loadResults();
+
+			expect(server.requests.length).toEqual(1);
+			setTimeout(function () {
+				server.respond();
+			}, 100);
+
+			return promise.then(function () {
+				expect(onReceivedStub).toHaveBeenCalledOnceWith(null, "RESULTS");
+			})
+		}
 	}
 
 });
