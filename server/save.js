154e04180ec7a715c7a80fe143a8edd3df2a376a
Dominykas Blyžė <hello@dominykas.com>
2014-01-14 11:11:47 +0200

    delete results file on save

--- server/save.js
+++ server/save.js
@@ -3,6 +3,13 @@ var fs = require("fs");
 module.exports = function (req, res) {
 	var fileName = req.body.file;
 	var text = req.body.text;
+	var delBrowser = parseInt(fileName.substr(0, 3), 10) <= 12;
+	var resFn = "build/" + (delBrowser ? "browser" : "node") + ".results.html";
+	if (fs.existsSync(resFn)) {
+		console.log("Deleting " + resFn);
+		fs.unlink(resFn);
+	}
+
 	if (fileName.substr(fileName.length - 8) == ".test.js") {
 
 		console.log("Test received, saving: " + fileName);
@@ -11,10 +18,11 @@ module.exports = function (req, res) {
 	} else {
 
 		console.log("Implementation received: " + fileName);
-		var outF = parseInt(fileName.substr(0, 3), 10) <= 12 ? "client/resultViewer.js" : "server/results.js";
+		var outF = delBrowser ? "client/resultViewer.js" : "server/results.js";
 		console.log("Saving as " + outF);
 		fs.writeFileSync(outF, text);
 
 	}
+
 	res.send("OK");
 };
